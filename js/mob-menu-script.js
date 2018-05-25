568572ef15f2c4d77145bc9bd3fc46c74fbf237d
bambalamua <39627942+bambalamua@users.noreply.github.com>
2018-05-25 22:12:39 +0300

    1
    1

---
+++ js/mob-menu-script.js
@@ -0,0 +1,9 @@
+function mobBtn(id) {
+    var div = document.getElementById('mobMenu');
+    if(div.style.display == 'block') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+}
